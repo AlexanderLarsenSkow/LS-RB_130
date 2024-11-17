e79e284541ed0e1a721a3e99d08296bed67efe88
Alex Skow <askow2015@gmail.com>
2024-11-17 00:09:16 +0000

    Added test assertions for change method in Cash Register

--- exercises/medium_testing/test.rb
+++ exercises/medium_testing/test.rb
@@ -9,9 +9,24 @@ class TestCashRegister < Minitest::Test
     transaction.prompt_for_payment
 
     register = CashRegister.new(500)
-
     register.accept_money(transaction)
 
     assert_equal(550, register.total_money)
   end
+
+  def test_change
+    register = CashRegister.new(1000)
+
+    my_wallet = 200
+    transaction = Transaction.new(100)
+    transaction.amount_paid = 120
+    my_wallet -= transaction.amount_paid
+
+    my_wallet += register.change(transaction)
+
+    assert_equal(100, my_wallet)
+    assert_equal(20, register.change(transaction))
+  end
+  
+  
 end
