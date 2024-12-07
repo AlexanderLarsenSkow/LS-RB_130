9107b2547204aeb6d11f30006a747d07a8c44319
Alex Skow <askow2015@gmail.com>
2024-12-07 05:46:58 +0000

    Added another part to return_proc

--- test_prep/blocks/return_proc.rb
+++ test_prep/blocks/return_proc.rb
@@ -9,3 +9,22 @@ c = proc_maker { puts :nononono }
 a.call
 b.call
 c.call
+
+def letter_maker
+  letter_code = 64
+  letters = ''
+
+  proc do
+    letter_code == 90 ? letter_code = 65 : letter_code += 1
+    letters << letter_code.chr
+  end
+end
+
+first = letter_maker
+3.times { first.call }
+
+second = letter_maker
+2.times { second.call }
+
+p first.call
+p second.call
